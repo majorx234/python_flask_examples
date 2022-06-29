194ebd5d7321d61a88df1a883d3c6230882b5b08
CodingWithLaura <laura.ganss@web.de>
2022-06-29 10:30:02 +0200

    Try to establish database connection at endpoint

--- resources/persons_endpoint.py
+++ resources/persons_endpoint.py
@@ -9,14 +9,29 @@ persons = [{'id': '1',
 api = Namespace('persons_endpoint',
                 description='persons information')
 
-
 @api.route('')
 class PersonEntpoint(Resource):
     def __init__(self, test):
         print("endpoint called")
+        try:
+            conn = psycopg2.connect("dbname='persons' user='laura' host='localhost'")
+        except:
+            print("connection could not be established")
+            cur = conn.cursor()
 
     def get(self):
         ''' send status '''
+        cur.execute("""SELECT * FROM person_profession """)
+        rows = cur.fetchall()
+        persons = []
+        for row in rows:
+            length = len(row)
+            person_obj = dict()
+            for i in range(0,length):
+                person_obj[person(i)] = row[i]
+                persons.append(person_obj)
+                print("")
+
         # ToDo:jsonfy #jsonify(persons)
         return persons, HTTPStatus.OK
 
